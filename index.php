79db03e14747607a3b37ec287f7e0a1c17e4b7b4
carlos soliz <johndoe@example.com>
2024-08-19 21:08:14 -0300

    v3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assect/layout.css">      
+    <link rel="stylesheet" href="./assect/layout.css"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
@@ -27,8 +27,7 @@
             <ion-icon name="cart-outline" class="size-full"></ion-icon>
         </button>
     </main>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
     <script>
         const btn_carrito = document.getElementById("btn-carrito")
         const cerrar_carrito = document.querySelector(".cerrar_carrito")
@@ -67,5 +66,7 @@
     <?php 
        require "lib/footer.php"
     ?>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
